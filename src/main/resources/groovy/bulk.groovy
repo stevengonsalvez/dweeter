a9210a032d777b51758a8742f823db84d2f51133
steven gonsalvez <steven.gonsalvez@gmail.com>
2016-08-08 21:04:59 +0100

    steve: latest changes}

--- src/main/resources/groovy/bulk.groovy
+++ src/main/resources/groovy/bulk.groovy
@@ -11,23 +11,27 @@ def thangie = message.getProperty('thang',PropertyScope.SESSION );
 
 //def staticString =  '{"index":{"_index":"dweetie", "_type":"'+ thangie +'"}}'
 def staticString =  '{"update":{"_index":"dweetie", "_retry_on_conflict" : 3, "_type":"'+ thangie +'", "_id":"'
-def docPre = '{ "doc" :'
-def docPost = ', "doc_as_upsert" : true }'
+def docPre = '{ "doc" : { "wrapper": '
+def docPost = '}, "doc_as_upsert" : true }'
 
-
+String compStr;
 String test;
 for (java.util.HashMap item : idList) {
-
+    def geoMap = [:]
+    
+    geoMap.lat = item.content.your_latitude
+    geoMap.lon = item.content.your_longitude
+    
     if (test == null){
         test =   staticString + item.created + '"}}' + '\n' +
                 docPre +
-                new JsonBuilder(item).toString() +  docPost + '\n'
+                new JsonBuilder(item).toString() + ',"pin": '+ new JsonBuilder(geoMap).toString() + docPost + '\n'
     }
     else
     {
         test = test + staticString + item.created + '"}}' + '\n' +
                 docPre +
-                new JsonBuilder(item).toString() +  docPost + '\n'
+                new JsonBuilder(item).toString() + ',"pin": '+ new JsonBuilder(geoMap).toString() +  docPost + '\n'
     }
 }
 
